7dd200a9b1bf954cff4fde8085a47af4dd760010
paw <pawrequest@users.noreply.github.com>
2024-04-28 01:46:31 +0100

    init imports

--- src/scrapaw/__init__.py
+++ src/scrapaw/__init__.py
@@ -1,9 +1,12 @@
-from .dtg import EpisodeBase
+from .dtg import EpisodeBase, episode_generator
 from .writers import HtmlWriter, RPostWriter, RWikiWriter
+from .scrapaw_config import ScrapawConfig
 
 __all__ = [
     'EpisodeBase',
     'HtmlWriter',
     'RPostWriter',
     'RWikiWriter',
+    'ScrapawConfig',
+    'episode_generator',
 ]
